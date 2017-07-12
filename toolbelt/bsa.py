f96dcb96eb171848ee8ddbc4c02bd45e62cf7f14
AJ Wilson <aj.wilson@scouting.org>
2017-07-12 16:13:43 -0500

    fix_base_document

--- toolbelt/bsa.py
+++ toolbelt/bsa.py
@@ -14,8 +14,8 @@ def fix_bsa_email(email_in):
 	"""
 	Read and correct the various common errors of emails in the BSA servers
 	"""
-	#Only work with lowercase strings
-	email+in = email_in.lower()
+	#Only work with lower-case emails
+	email_in = email_in.lower()
 
 	#Regular Expression To Use:
 	regex =r"""^(((([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$"""
@@ -42,15 +42,14 @@ def fix_bsa_email(email_in):
 				return(re_match.group(0))
 
 		else:
-			#retrieve dataframe of domains from cwd to compare
+			#retrieve list of domains from cwd to compare
 			domains = pd.read_pickle(os.path.join(__location__,"domains.pickle"))
 
 			#split user input on the '@' character, length must be 2
 			_split = email_in.split('@')
 			if len(_split) == 2:
-				#apply levenshtein_distance to domains df
 				domains['repl_proba'] = (domains['probability']) / (domains['domain'].apply(levenshtein_distance, s2 = _split[1])**2.71828)
-                best_alt_domain = domains.sort_values(by='repl_proba', ascending = False).iloc[0]['domain']
+				best_alt_domain = domains.sort_values(by='repl_proba', ascending = False).iloc[0]['domain']
 
 				#if changing domain did not suffice to fix the problem, do not return the new domain
 				if re.match(regex, _split[0]+"@"+best_alt_domain):
