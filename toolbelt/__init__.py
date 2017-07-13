b05e9dc0f65ee744fe1250ab1daa065504b0e973
AJ Wilson <aj.wilson@scouting.org>
2017-07-13 07:37:33 -0500

    version update

--- toolbelt/__init__.py
+++ toolbelt/__init__.py
@@ -3,7 +3,7 @@ import os
 
 __author__ = """AJ Wilson"""
 __email__ = """aj.wilson08@gmail.com"""
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
--- toolbelt/deprecated_bsa.py
+++ toolbelt/deprecated_bsa.py
@@ -3,7 +3,6 @@
 import os
 import string
 import re
-import pandas as pd
 from jellyfish import levenshtein_distance
 from .utils import validate_str
 
@@ -15,9 +14,6 @@ def fix_bsa_email(email_in):
 	"""
 	Read and correct the various common errors of emails in the BSA servers
 	"""
-	#Only work with lower-case emails
-	email_in = email_in.lower()
-
 	#Regular Expression To Use:
 	regex =r"""^(((([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-zA-Z]|\d|-|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-zA-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$"""
 
@@ -44,14 +40,20 @@ def fix_bsa_email(email_in):
 
 		else:
 			#retrieve list of domains from cwd to compare
-			domains = pd.read_pickle(os.path.join(__location__,"domains.pickle"))
-
+			with open(os.path.join(__location__,"domains_list.txt"),'r') as f:
+				domains = f.read().split('\n')
 			#split user input on the '@' character, length must be 2
 			_split = email_in.split('@')
 			if len(_split) == 2:
-				domains['repl_proba'] = (domains['probability']) / (domains['domain'].apply(levenshtein_distance, s2 = _split[1])**2.71828)
-				best_alt_domain = domains.sort_values(by='repl_proba', ascending = False).iloc[0]['domain']
-
+				best_alt_domain = ''
+				_distance = 999
+				#look for alternate, possibly correct domain structure in the list
+				for domain in domains:
+					this_dist = levenshtein_distance(_split[1],domain)
+					#replace given domain with closeset by levenshtein_distance
+					if this_dist < _distance:
+						best_alt_domain = domain
+						_distance = this_dist
 				#if changing domain did not suffice to fix the problem, do not return the new domain
 				if re.match(regex, _split[0]+"@"+best_alt_domain):
 					return(_split[0]+"@"+best_alt_domain)
