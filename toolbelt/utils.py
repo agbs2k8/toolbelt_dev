0d083d8ed2fb4d9b1cf1e5b2fb6c2d36a04a0331
AJ Wilson <aj.wilson@scouting.org>
2017-07-18 15:22:38 -0500

    added @validate_list

--- toolbelt/utils.py
+++ toolbelt/utils.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
+
 def validate_df(func):
-	"""
-	A decorator function to validate if the input is a pandas data frame
-	:param func: a function to validate
-	"""
-	
-	@wraps(func)
-	def wrapper(*args, **kwargs):
-		if isinstance(args[0], pd.DataFrame):
-			return func(*args,**kwargs)
-		else:
-			raise TypeError('This tool only supports input as a DataFrame')
-	return wrapper
-	
+    """
+    A decorator function to validate if the input is a pandas data frame
+    :param func: a function to validate
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if isinstance(args[0], pd.DataFrame):
+            return func(*args,**kwargs)
+        else:
+            raise TypeError('This tool only supports input as a DataFrame')
+    return wrapper
+
+
 def validate_str(func):
     """
     A decorator function to validate if the input is string
@@ -26,4 +28,18 @@ def validate_str(func):
             return func(*args, **kwargs)
         else:
             raise TypeError('This tool only supports input as a string')
+    return wrapper
+
+
+def validate_list(func):
+    """
+    A decorator function to validate if the input is list
+    :param func: a function to validate
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if isinstance(args[0], list):
+            return func(*args, **kwargs)
+        else:
+            raise TypeError('This tool only supports input as a list')
     return wrapper
